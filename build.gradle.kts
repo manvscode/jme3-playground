858ee2f01ecb575dabc2518109f9f588e54928ef
Joe Marrero <joe.marrero@smiledirectclub.com>
2019-08-14 13:53:01 -0400

    Emptry project.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+}
+
+group = "com.joemarrero"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+val jmeVersion: String = "3.2.0-stable"
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+
+    dependencies {
+        compile("org.jmonkeyengine:jme3-core:${jmeVersion}")
+        runtime("org.jmonkeyengine:jme3-desktop:${jmeVersion}")
+        runtime("org.jmonkeyengine:jme3-lwjgl:${jmeVersion}")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+
+}
